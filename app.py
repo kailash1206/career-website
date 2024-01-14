77c0ed965b8083c7c278332e0a2d9041e31dc819
kailash1206 <132139925+kailash1206@users.noreply.github.com>
2024-01-14 09:56:36 +0000

    added HTML & CSS

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from logging import debug
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='Templates')
 
 
 @app.route("/")
-def hello_world():
-  return "Hello, Kailash"
+def hello_kailash():
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
-  app.run(port=8080, host="0.0.0.0", debug=True)
+  app.run(port=8080, host='0.0.0.0', debug=True)
